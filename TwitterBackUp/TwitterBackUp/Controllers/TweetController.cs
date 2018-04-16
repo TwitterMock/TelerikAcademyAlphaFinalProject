0d4556b7a9ba7fa812f7946c9876734e7cc47518
cheficha <stefanzhekov95@gmail.com>
2018-04-16 19:10:03 +0300

    Created SingleTweetDTO,Created Controller for tweets,created Search Model and SearchViewModel

---
+++ TwitterBackUp/TwitterBackUp/Controllers/TweetController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace TwitterBackUp.Controllers
+{
+ 
+    
+    public class TweetController : Controller
+    {
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]       
+        public IActionResult Search(string text)
+        {
+            ViewData["text"]=text;
+
+            return View();
+        }
+    }
+}
