3d7d57f7611a7c495d229ad4485ae94dad393ee4
cheficha <stefanzhekov95@gmail.com>
2018-04-19 13:39:49 +0300

    Timeline service done

---
+++ TwitterBackUp/TwitterBackUp.DTO/TweetsTimeline/TweetUrls.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwitterBackUp.DTO.TweetsTimeline
+{
+    public class TweetUrls
+    {
+        [JsonProperty("url")]
+        public string Url { get; set; }
+    }
+}
