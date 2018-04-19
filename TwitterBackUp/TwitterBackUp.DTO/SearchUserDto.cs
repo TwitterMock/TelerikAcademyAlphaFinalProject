a9500adb8ef6d3e5c5e13602fd742a00d8819e66
mstilyan <stilyan.mladenov@gmail.com>
2018-04-19 12:10:37 +0300

    SearchUserDto added

---
+++ TwitterBackUp/TwitterBackUp.DTO/SearchUserDto.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+
+namespace TwitterBackUp.DTO
+{
+    public class SearchUserDto
+    {
+        [JsonProperty("id_str")]
+        public string UserId { get; set; }
+        [JsonProperty("name")]
+        public string Username { get; set; }
+        [JsonProperty("screen_name")]
+        public string ScreenName { get; set; }
+        [JsonProperty("description")]
+        public string Description { get; set; }
+        [JsonProperty("url")]
+        public string Url { get; set; }
+        [JsonProperty("followers_count")]
+        public int FollowersCount { get; set; }
+        [JsonProperty("friends_count")]
+        public int FriendsCount { get; set; }
+        [JsonProperty("profile_image_url")]
+        public string ProfileImageUrl { get; set; }
+        [JsonProperty("profile_background_image_url")]
+        public string ProfileBackgroundImageUrl { get; set; }
+    }
+}
