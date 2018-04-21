e42845319f591e9a7777cd268f1c2a21e70d6c24
mstilyan <stilyan.mladenov@gmail.com>
2018-04-21 09:24:50 +0300

    DataModels created

---
+++ TwitterBackUp/TwitterBackUp.DataModels/Configurations/TweetFluentConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TwitterBackUp.DataModels.Configurations.Contracts;
+using TwitterBackUp.DomainModels;
+
+namespace TwitterBackUp.DataModels.Configurations
+{
+    public class TweetFluentConfiguration : IFluentConfiguration
+    {
+        public void Register(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Tweet>()
+                .HasMany(x => x.TwitteTweets)
+                .WithOne(x => x.Tweet)
+                .HasForeignKey(x => x.TweetId);
+
+            modelBuilder.Entity<Tweet>()
+                .HasKey(x => x.TweetId);
+
+            modelBuilder.Entity<Tweet>()
+                .Property(x => x.TweetId)
+                .ValueGeneratedNever();
+        }
+    }
+}
---
+++ TwitterBackUp/TwitterBackUp.DataModels/Configurations/TwitterFluentConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TwitterBackUp.DataModels.Configurations.Contracts;
+using TwitterBackUp.DomainModels;
+
+namespace TwitterBackUp.DataModels.Configurations
+{
+    public class TwitterFluentConfiguration : IFluentConfiguration
+    {
+        public void Register(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Twitter>()
+                .HasMany(x => x.TwitteTweets)
+                .WithOne(x => x.Twitter)
+                .HasForeignKey(x => x.TwitterId);
+
+            modelBuilder.Entity<Twitter>()
+                .HasKey(x => x.UserId);
+
+            modelBuilder.Entity<Twitter>()
+                .Property(x => x.UserId)
+                .ValueGeneratedNever();
+        }
+    }
+}
